8afea5c560a624fce11da40ab5c8dd15e3488af1
hevienz <hevienz@qq.com>
2020-06-17 13:18:34 +0000

    add debug info

--- tc/tcp_drop.c
+++ tc/tcp_drop.c
@@ -1,18 +1,27 @@
-// tc-xdp-drop-tcp.c
 #include <stdbool.h>
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/in.h>
 #include <linux/pkt_cls.h>
+#include <stdio.h>
 
 #include "bpf_endian.h"
 #include "bpf_helpers.h"
 
+typedef unsigned int    u32;
+#define bpfprint(fmt, ...)                        \
+    ({                                             \
+        char ____fmt[] = fmt;                      \
+        bpf_trace_printk(____fmt, sizeof(____fmt), \
+                         ##__VA_ARGS__);           \
+    })
+
 /*
   check whether the packet is of TCP protocol
 */
-static bool is_TCP(void *data_begin, void *data_end){
+static __inline bool is_TCP(void *data_begin, void *data_end){
+  bpfprint("Entering is_TCP\n");
   struct ethhdr *eth = data_begin;
 
   // Check packet's size
@@ -28,11 +37,21 @@ static bool is_TCP(void *data_begin, void *data_end){
     if ((void *)(iph + 1) > data_end)
       return false;
 
+    // extract src ip and destination ip
+    u32 ip_src = iph->saddr;
+    u32 ip_dst = iph->daddr;
+    
+    // 
+    bpfprint("src ip addr1: %d.%d.%d\n",(ip_src) & 0xFF,(ip_src >> 8) & 0xFF,(ip_src >> 16) & 0xFF);
+    bpfprint("src ip addr2:.%d\n",(ip_src >> 24) & 0xFF);
+
+    bpfprint("dest ip addr1: %d.%d.%d\n",(ip_dst) & 0xFF,(ip_dst >> 8) & 0xFF,(ip_dst >> 16) & 0xFF);
+    bpfprint("dest ip addr2: .%d\n",(ip_dst >> 24) & 0xFF);
+
     // Check if IP packet contains a TCP segment
     if (iph->protocol == IPPROTO_TCP)
       return true;
   }
-
   return false;
 }
 
@@ -53,13 +72,15 @@ SEC("tc")
 int tc_drop_tcp(struct __sk_buff *skb)
 {
 
+  bpfprint("Entering tc section\n");
   void *data = (void *)(long)skb->data;
   void *data_end = (void *)(long)skb->data_end;
 
-  if (is_TCP(data, data_end)) 
-    return TC_ACT_SHOT;
 
-  return TC_ACT_OK;
+  if (is_TCP(data, data_end))
+    return TC_ACT_SHOT;
+  else
+    return TC_ACT_OK;
 }
 
 char _license[] SEC("license") = "GPL";
